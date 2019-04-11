289fd3b5f3d694c0486e9d3432d8b756264ab373
Gorka <gorka.molero@gmail.com>
2019-04-11 16:51:46 +0200

    Airtable & prep

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,14 @@
+const dotenv = require("dotenv")
+const path = require('path')
+
+if (process.env.ENVIRONMENT !== "production") dotenv.config()
+const production = process.env
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Q T Z L`,
+    description: `Transatlantic Musical Collective`,
+    author: `@gorkamolero`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -30,5 +36,27 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.GATSBY_AIRTABLE_API_KEY, // may instead specify via env, see below
+        tables: [
+          {
+            baseId: `apptg1boOr10IkCsY`,
+            tableName: `Releases`,
+            //tableView: `YOUR_TABLE_VIEW_NAME`, // optional
+            //queryName: `OPTIONAL_NAME_TO_IDENTIFY_TABLE`, // optional
+            //mapping: { `CASE_SENSITIVE_COLUMN_NAME`: `VALUE_FORMAT` }, // optional, e.g. "text/markdown", "fileNode"
+            //tableLinks: [`CASE`, `SENSITIVE`, `COLUMN`, `NAMES`] // optional, for deep linking to records across tables.
+          },
+         /*  {
+            baseId: `apptg1boOr10IkCsY`,
+            tableName: `YOUR_TABLE_NAME`,
+            tableView: `YOUR_TABLE_VIEW_NAME` // optional
+            // can leave off queryName, mapping or tableLinks if not needed
+          } */
+        ]
+      }
+    }
   ],
 }
